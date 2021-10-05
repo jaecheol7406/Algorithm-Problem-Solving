1e3a99536642417969d8f0af4843a5bb22dafed2
안재철 <qnzo8888@gmail.com>
2021-10-06 01:00:11 +0900

    Create 전화번호 목록.java

---
+++ 프로그래머스/전화번호 목록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for(int i = 0; i < phone_book.length - 1; i++){
+            String now = phone_book[i];
+            String next = phone_book[i + 1];
+            if(next.startsWith(now))
+                return false;
+        }
+        return true;
+    }
+}
