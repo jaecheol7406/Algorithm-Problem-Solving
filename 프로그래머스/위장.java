5ba7942be81af6be99f4409b1b81bc3a4be1dc77
안재철 <qnzo8888@gmail.com>
2021-10-06 00:52:58 +0900

    Update 위장.java

--- 프로그래머스/위장.java
+++ 프로그래머스/위장.java
@@ -1,3 +1,20 @@
-public class 위장 {
-    
+import java.util.*;
+class Solution {
+    HashMap<String, Integer> map = new HashMap<>();
+    public int solution(String[][] clothes) {
+        for(String[] cl : clothes){
+            String k = cl[1];
+            String v = cl[0];
+            
+            map.put(k, map.getOrDefault(k, 0) + 1);
+        }
+        
+        int mul = 1;
+        for(String k : map.keySet()){
+            int v = map.get(k);
+            mul *= (v + 1);
+        }
+        
+        return mul - 1;
+    }
 }
