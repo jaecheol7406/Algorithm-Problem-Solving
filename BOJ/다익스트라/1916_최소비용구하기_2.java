f4ba72213d4932977f277ab71eb0287a55c23512
jaecheol7406 <57355410+jaecheol7406@users.noreply.github.com>
2022-02-08 14:31:35 +0900

    Create 1916_최소비용구하기_2.java

---
+++ BOJ/다익스트라/1916_최소비용구하기_2.java
@@ -0,0 +1,85 @@
+package _2022_02;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class No_1916_최소비용구하기 {
+	static int INF = 99999999;
+	static int N, M;
+	static int[] dist;
+	static ArrayList<Node>[] list;
+	
+	static class Node implements Comparable<Node> {
+		int v, w;
+		
+		public Node(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
+		
+		public int compareTo(Node n) {
+			return this.w - n.w;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = toInt(br.readLine());
+		M = toInt(br.readLine());
+		
+		dist = new int[N + 1];
+		Arrays.fill(dist, INF);
+		
+		list = new ArrayList[N + 1];
+		for(int i = 1; i <= N; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < M; i++) {
+			String[] in = br.readLine().split(" ");
+			int a = toInt(in[0]);
+			int b = toInt(in[1]);
+			int w = toInt(in[2]);
+			
+			list[a].add(new Node(b, w));
+		}
+		
+		String[] in = br.readLine().split(" ");
+		int from = toInt(in[0]);
+		int to = toInt(in[1]);
+		
+		dijkstra(from);
+		System.out.println(dist[to]);
+	}
+	
+	static void dijkstra(int from) {
+		dist[from] = 0;
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(from, 0));
+		
+		boolean[] check = new boolean[N + 1];
+		
+		while(!pq.isEmpty()) {
+			int now = pq.poll().v;
+			if(check[now])
+				continue;
+			check[now] = true;
+			
+			for(Node node : list[now]) {
+				int next = node.v;
+				int val = node.w;
+				
+				if(dist[next] > dist[now] + val) {
+					dist[next] = dist[now] + val;
+					pq.add(new Node(next, dist[next]));
+				}
+			}
+		}
+	}
+	
+	static int toInt(String s) {
+		return Integer.parseInt(s);
+	}
+}
